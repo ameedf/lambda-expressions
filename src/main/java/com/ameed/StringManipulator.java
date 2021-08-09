dadae9efaeaeca78d97afd5fa6d1b3c7517e012a
Ameed <ameed.software@gmail.com>
2021-08-09 23:13:57 +0300

    Adding "andThen"

--- src/main/java/com/ameed/StringManipulator.java
+++ src/main/java/com/ameed/StringManipulator.java
@@ -2,4 +2,24 @@ package com.ameed;
 
 public interface StringManipulator {
     String manipulate(String text1, String text2);
+
+    default StringManipulator andThen(StringManipulator next) {
+        return new StringManipulatorWrapper(this, next);
+    }
+
+    class StringManipulatorWrapper implements StringManipulator {
+        private final StringManipulator first;
+        private final StringManipulator second;
+
+        private StringManipulatorWrapper(StringManipulator first, StringManipulator second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public String manipulate(String text1, String text2) {
+            String resultOf1 = first.manipulate(text1, text2);
+            return second.manipulate(resultOf1, text2);
+        }
+    }
 }
